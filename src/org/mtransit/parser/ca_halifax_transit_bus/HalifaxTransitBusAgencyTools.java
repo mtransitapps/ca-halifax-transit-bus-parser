b63da4973744768ffa1fce057f3243d37e20c9ee
Mathieu Méa <mathieu.mea@gmail.com>
2015-05-22 08:47:21 -0400

    Compatibility with shared parser library & code cleaning.

--- src/org/mtransit/parser/ca_halifax_transit_bus/HalifaxTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_halifax_transit_bus/HalifaxTransitBusAgencyTools.java
@@ -11,6 +11,7 @@ import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
@@ -326,7 +327,7 @@ public class HalifaxTransitBusAgencyTools extends DefaultAgencyTools {
 	private static final String PORTLAND_HLS = "Portland Hls";
 
 	@Override
-	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip) {
+	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
 		if (mRoute.id == 1l) {
 			if (gTrip.direction_id == 1) {
 				mTrip.setHeadsignString(BRIDGE_TERMINAL, gTrip.direction_id);

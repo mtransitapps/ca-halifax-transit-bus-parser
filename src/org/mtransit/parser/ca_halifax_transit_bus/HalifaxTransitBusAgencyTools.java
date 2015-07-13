5e2b8be72456eb52dedfb096f6097dd3c6e41bc2
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 16:58:24 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_halifax_transit_bus/HalifaxTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_halifax_transit_bus/HalifaxTransitBusAgencyTools.java
@@ -38,7 +38,7 @@ public class HalifaxTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating Halifax Transit bus data...\n");
+		System.out.printf("\nGenerating Halifax Transit bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
